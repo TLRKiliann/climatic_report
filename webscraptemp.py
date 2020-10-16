78cafade582d1ebf98d3355c19e294e9255b5655
TLRKiliann <philogenie@protonmail.com>
2020-10-16 11:25:51 +0200

    temp for january 2011

--- webscraptemp.py
+++ webscraptemp.py
@@ -6,13 +6,13 @@ from bs4 import BeautifulSoup
 import requests
 
 
-url = "https://www.historique-meteo.net/europe/suisse/lausanne/2020/08/"
+url = "https://www.historique-meteo.net/europe/suisse/lausanne/2016/12/"
 tds=[]
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
 #print(soup.text)
 divs = soup.findAll("table", {"class": "table table-striped"})
-with open('myscrap.txt', '+w') as file:
+with open('myscrap_dec_16.txt', '+w') as file:
     for div in divs:
         rows = div.findAll('td')
         for row in rows:
