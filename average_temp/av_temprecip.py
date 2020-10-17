6e7b09d242074cc1d7b71a736be973646bc9a12e
TLRKiliann <philogenie@protonmail.com>
2020-10-17 21:20:16 +0200

    presentation improved

--- average_temp/av_temprecip.py
+++ average_temp/av_temprecip.py
@@ -21,8 +21,6 @@ precip_2016 = [80, 69, 53, 34, 63, 70, 58, 39, 25, 29, 46, 1]
 
 precip_2020 = [117, 244, 190, 88, 163, 291, 200, 247, 143, None, None, None]
 
-
-
 style = dict(color="C4", linewidth=1, linestyle='--', marker='.', mec="C5")
 style2 = dict(color="C4", linewidth=1, linestyle='--', marker='.', mec="C6")
 style3 = dict(color="C4", linewidth=1, linestyle='--', marker='.', mec="C9")
@@ -38,32 +36,38 @@ with plt.style.context(("seaborn-darkgrid")):
     #axes[0].set_xlabel('Dates', fontsize=12)
     axes[0].set_ylabel('Average of temperatures (°C)', fontsize=12)
     axes[0].set_title('2011')
+    axes[0].legend(['2011']) 
     axes[0].grid(color='w', linestyle='-', linewidth=1)
 
     axes[1].plot(list_date, list_2016, **style2)
     #axes[1].set_xlabel('Dates', fontsize=12)
     axes[1].set_title('2016')
+    axes[1].legend(['2016']) 
     axes[1].grid(color='w', linestyle='-', linewidth=1)
 
     axes[2].plot(list_date, list_2020, **style3)
     #axes[2].set_xlabel('Dates', fontsize=12)
     axes[2].set_title('2020')
+    axes[2].legend(['2020']) 
     axes[2].grid(color='w', linestyle='-', linewidth=1)
 
     axes2[0].plot(list_date, precip_2011, **style4)
     axes2[0].set_xlabel('Dates', fontsize=12)
     axes2[0].set_ylabel('Total precipitations (mm)', fontsize=12)
     #axes2[0].set_title('2011')
+    axes2[0].legend(['2011']) 
     axes2[0].grid(color='w', linestyle='-', linewidth=1)
 
     axes2[1].plot(list_date, precip_2016, **style5)
     axes2[1].set_xlabel('Dates', fontsize=12)
     #axes2[1].set_title('2016')
+    axes2[1].legend(['2016']) 
     axes2[1].grid(color='w', linestyle='-', linewidth=1)
 
     axes2[2].plot(list_date, precip_2020, **style6)
     axes2[2].set_xlabel('Dates', fontsize=12)
     #axes2[2].set_title('2020')
+    axes2[2].legend(['2020']) 
     axes2[2].grid(color='w', linestyle='-', linewidth=1)
 
     plt.show()
