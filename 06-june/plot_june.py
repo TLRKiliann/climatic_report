bd45ff2991b91724b124c0a7cc3a76fe17cd7a8d
TLRKiliann <philogenie@protonmail.com>
2020-10-18 18:13:19 +0200

    plot_June fnish

--- 06-june/plot_june.py
+++ 06-june/plot_june.py
@@ -4,30 +4,32 @@
 
 import matplotlib.pyplot as plt
 
-list_date = ['01/01', '02/01', '03/01',
-'04/01', '05/01', '06/01', '07/01',
-'08/01', '09/01', '10/01', '11/01',
-'12/01', '13/01', '14/01', '15/01',
-'16/01', '17/01', '18/01', '19/01',
-'20/01', '21/01', '22/01', '23/01',
-'24/01', '25/01', '26/01', '27/01',
-'28/01', '29/01', '30/01', '31/01']
 
-list_2011 = []
+list_date = ['01', '02', '03',
+'04', '05', '06', '07',
+'08', '09', '10', '11',
+'12', '13', '14', '15',
+'16', '17', '18', '19',
+'20', '21', '22', '23',
+'24', '25', '26', '27',
+'28', '29', '30']
 
-list_2016 = []
+list_2011 = [11,16,19,20,19,18,18,12,15,16,13,18,18,19,21,22,20,13,14,19,23,21,16,16,20,24,21,26,21,17]
 
-list_2020 = []
+list_2016 = [16,13,16,17,18,19,20,19,18,14,17,15,16,13,11,19,16,16,15,18,18,23,25,25,20,18,19,21,23,21]
+
+list_2020 = [21,22,21,16,14,17,12,17,13,14,18,21,19,16,17,18,17,18,17,20,20,22,23,25,25,25,25,22,22,23]
 
 show_grid = True
 with plt.style.context('seaborn-darkgrid'):
-    plt.plot(list_date, list_2011, 'ro--')
+    plt.plot(list_date, list_2011, 'co--')
     plt.plot(list_date, list_2016, 'bo--')
-    plt.plot(list_date, list_2020, 'co-')
-    plt.ylabel('T°C')
-    plt.xlabel('Dates')
+    plt.plot(list_date, list_2020, 'mo-')
+    plt.ylabel('Temperatures (°C)', fontsize=14)
+    plt.xlabel('Days', fontsize=14)
     plt.xticks(rotation=45)
-    plt.title('Comparison of temperatures for january after-noon')
+    plt.title('Temperatures for June 2011-2016-2020 (after-noon)',
+    	fontsize=16)
     plt.legend(['temp 2011', 'temp 2016', 'temp 2020'])
     plt.grid(show_grid)
 
