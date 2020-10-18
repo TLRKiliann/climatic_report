7730428682d0b408801156fa4aa216a037660dc0
TLRKiliann <philogenie@protonmail.com>
2020-10-18 18:25:44 +0200

    plot_July finish

--- 07-july/plot_july.py
+++ 07-july/plot_july.py
@@ -4,30 +4,32 @@
 
 import matplotlib.pyplot as plt
 
-list_date = ['01/01', '02/01', '03/01',
-'04/01', '05/01', '06/01', '07/01',
-'08/01', '09/01', '10/01', '11/01',
-'12/01', '13/01', '14/01', '15/01',
-'16/01', '17/01', '18/01', '19/01',
-'20/01', '21/01', '22/01', '23/01',
-'24/01', '25/01', '26/01', '27/01',
-'28/01', '29/01', '30/01', '31/01']
 
-list_2011 = []
+list_date = ['01', '02', '03',
+'04', '05', '06', '07',
+'08', '09', '10', '11',
+'12', '13', '14', '15',
+'16', '17', '18', '19',
+'20', '21', '22', '23',
+'24', '25', '26', '27',
+'28', '29', '30', '31']
 
-list_2016 = []
+list_2011 = [17,16,18,21,23,22,21,21,23,21,22,26,20,16,17,20,14,15,16,15,16,18,15,13,17,18,18,18,19,18,18]
 
-list_2020 = []
+list_2016 = [23,18,20,22,22,21,23,25,24,26,24,19,15,13,17,19,23,21,22,22,21,21,20,21,21,22,23,22,24,26,21]
+
+list_2020 = [26,21,19,21,23,20,21,24,26,24,21,21,23,21,17,19,20,21,24,26,23,24,24,21,23,24,26,27,25,27,29]
 
 show_grid = True
 with plt.style.context('seaborn-darkgrid'):
-    plt.plot(list_date, list_2011, 'ro--')
+    plt.plot(list_date, list_2011, 'co--')
     plt.plot(list_date, list_2016, 'bo--')
-    plt.plot(list_date, list_2020, 'co-')
-    plt.ylabel('T°C')
-    plt.xlabel('Dates')
+    plt.plot(list_date, list_2020, 'ro-')
+    plt.ylabel('Temperatures (°C)', fontsize=14)
+    plt.xlabel('Days', fontsize=14)
     plt.xticks(rotation=45)
-    plt.title('Comparison of temperatures for january after-noon')
+    plt.title('Temperatures for July 2011-2016-2020 (after-noon)',
+    	fontsize=16)
     plt.legend(['temp 2011', 'temp 2016', 'temp 2020'])
     plt.grid(show_grid)
 
